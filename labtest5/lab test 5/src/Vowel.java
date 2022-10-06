26cd79ca4e5c2d7f44371cd525abc2fa72b1e8e7
ashwin1410-stack <110716420+ashwin1410-stack@users.noreply.github.com>
2022-10-06 18:21:47 +0530

    Add files via upload

---
+++ labtest5/lab test 5/src/Vowel.java
@@ -0,0 +1,26 @@
+
+public class Vowel {
+
+
+ static void main(String[] args)
+	{    
+         int vCount = 0, cCount = 0;    
+         String str = "Best Of Luck";     
+         str = str.toLowerCase();    
+            
+        for(int i = 0; i < str.length(); i++)
+        {    
+        if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') 
+        {     
+           vCount++;    
+         }    
+              
+            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z') {      
+                //Increments the consonant counter    
+                cCount++;    
+            }    
+        }    
+        System.out.println("Number of vowels: " + vCount);    
+        System.out.println("Number of consonants: " + cCount);    
+    }    
+}   
